865b4bc394c08edc0656e903fc7640edac6371ff
u1986f <u1986f@gmail.com>
2023-04-08 17:33:55 +0100

    Update code

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -1,13 +1,12 @@
 const Port = require('../src/port.js');
 
 describe('Port', () => {
-    it('returns an object', () => {
-      expect(new Port('')).toBeInstanceOf(Object);
-    });
+  it('returns an object', () => {
+    expect(new Port('')).toBeInstanceOf(Object);
+  })
 };
   
-    it('has a starting port', () => {
-      const Port = new Port('Starting Port');
-  
-      expect(port.name).toEqual('Starting Port');
-      });
+  it('has a starting port', () => {
+    const Port = new Port('Starting Port');
+  expect(port.name).toEqual('Starting Port');
+});
