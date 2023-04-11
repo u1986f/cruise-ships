7c013bf0ca95a4ec044a0773e7f627e46913a5af
u1986f <u1986f@gmail.com>
2023-04-11 18:41:59 +0100

    Tests for Ship and Ship constructor

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -3,6 +3,11 @@ const Ship = require('../src/Ship.js')
 
 describe('Ship', () => {
     it('can be instantiated', () => {
-        expect(new.Ship()).toBeinstanceOf(Object);
+        expect(new Ship()).toBeInstanceOf(Object);
+    });
+
+    it('has a starting point', () => {
+        const ship = new Ship('Dover');
+        expect(ship.startingPort).toBe('Dover');
     });
 });
